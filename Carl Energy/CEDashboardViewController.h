be01f3ac7474acf07b1801d8233fd85403454a26
Brian Charous <brian.charous@gmail.com>
2014-05-23 17:04:02 -0500

    Get wind data and draw on dashboard… eventually

--- Carl Energy/CEDashboardViewController.h
+++ Carl Energy/CEDashboardViewController.h
@@ -11,9 +11,21 @@
 #import "CorePlot-CocoaTouch.h"
 
 
-@interface CEDashboardViewController : UIViewController <CEDataRetrieverDelegate, CPTPlotDataSource>
+@interface CEDashboardViewController : UIViewController <CEDataRetrieverDelegate, CPTPlotDataSource> {
+    NSNumber *windProduction;
+    NSNumber *energyConsumption;
+    BOOL gotWindProduction;
+    BOOL gotElectricityUsage;
+    CPTXYGraph *pieChart;
+}
+
+- (void)makePieChart;
+- (void)getElectricProducionAndUsage;
+- (void)updatePieChart;
 
 @property (readwrite, strong, nonatomic) NSMutableArray *dataForChart;
 @property IBOutlet UIScrollView *scrollView;
+@property (nonatomic, strong) CPTGraphHostingView *hostView;
+@property (nonatomic, strong) CPTTheme *selectedTheme;
 
 @end

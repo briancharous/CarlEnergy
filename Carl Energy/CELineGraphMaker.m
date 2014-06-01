3d1a3b3902f7a72d8fc6503ef5d926826b3a45e6
Brian Charous <brian.charous@gmail.com>
2014-06-01 13:15:40 -0500

    Fix crash/bugs due to async getting new data being called multiple times if the user switches between graph time periods too fast

--- Carl Energy/CELineGraphMaker.m
+++ Carl Energy/CELineGraphMaker.m
@@ -53,11 +53,19 @@
             break;
     }
     
-    //TODO: cancel request if another one is in progress
     if (!self.retreiver.requestInProgress) {
-        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^ {
+        dispatch_async(dispatch_queue_create("com.carlenergy.graphs", NULL), ^ {
             [self.retreiver getUsage:type ForBuilding:building startTime:previous endTime:now resolution:resolution];
         });
+    }
+    else {
+        // if a request is in progress, cancel the request by deleteing the old
+        // data retreiver
+        // is this going to be a memory issue??
+        [self.retreiver setDelegate:nil];
+        self.retreiver = nil;
+        self.retreiver = [[CEDataRetriever alloc] init];
+        [self.retreiver setDelegate:self];
     }
 }
 
@@ -509,13 +517,13 @@
     plotSpace.yRange = yRange;
     
     // test code for y axis problem
-    NSLog(@"%@", self.y.title);
-    NSLog(@"%lu", (unsigned long)[self.y.axisLabels count]);
-    NSLog(@"%lu", (unsigned long)[self.y.majorTickLocations count]);
-    NSArray *yArray = [self.y.axisLabels allObjects];
-    NSLog(@"%@", yArray);
-    NSArray *yArray2 = [self.y.majorTickLocations allObjects];
-    NSLog(@"%@", yArray2);
+//    NSLog(@"%@", self.y.title);
+//    NSLog(@"%lu", (unsigned long)[self.y.axisLabels count]);
+//    NSLog(@"%lu", (unsigned long)[self.y.majorTickLocations count]);
+//    NSArray *yArray = [self.y.axisLabels allObjects];
+//    NSLog(@"%@", yArray);
+//    NSArray *yArray2 = [self.y.majorTickLocations allObjects];
+//    NSLog(@"%@", yArray2);
     
 }
 @end

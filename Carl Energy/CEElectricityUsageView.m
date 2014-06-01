78eba7bd24fb274eb2e12a710cf40fbc9ded9259
Larkin Flodin <flodinl@carleton.edu>
2014-06-01 12:40:07 -0500

    made small aesthetic changes to dashboard electricity stuff

--- Carl Energy/CEElectricityUsageView.m
+++ Carl Energy/CEElectricityUsageView.m
@@ -28,11 +28,11 @@
     [peakRetreiver setDelegate:self];
     
     NSDate *now = [NSDate date];
-    NSDate *shorlyBeforeNow = [now dateByAddingTimeInterval:-60*60*1.1]; // two hour ago (really only need the server to return 1 data point)
+    NSDate *shortlyBeforeNow = [now dateByAddingTimeInterval:-60*60*1.1]; // two hour ago (really only need the server to return 1 data point)
     
     gotInstantUsage = NO;
     dispatch_async(dispatch_queue_create("com.carlenergy.dashboard", NULL), ^ {
-        [instantRetreiver getTotalCampusElectricityUsageWithStartTime:shorlyBeforeNow endTime:now resolution:kResolutionHour];
+        [instantRetreiver getTotalCampusElectricityUsageWithStartTime:shortlyBeforeNow endTime:now resolution:kResolutionHour];
     });
     
     gotPeakUsage = NO;
@@ -49,8 +49,9 @@
         return;
     }
     
+    self.peakLabel.numberOfLines = 0;
     NSString *instantString = [NSString stringWithFormat:@"%li kW currently being used", (long)instantUsage];
-    NSString *maxString = [NSString stringWithFormat:@"%li kW was the peak consumption for this month", (long)peakUsage];
+    NSString *maxString = [NSString stringWithFormat:@"%li kW was the peak consumption \n for this month", (long)peakUsage];
     [self.currentLabel setText:instantString];
     [self.peakLabel setText:maxString];
 }
